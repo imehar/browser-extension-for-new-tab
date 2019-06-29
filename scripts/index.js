4b44e7a08156e17892dc70d35348265982cfd58c
Mehar Madnani <41502248+imehar@users.noreply.github.com>
2019-06-29 23:52:14 +0530

    browser local storage

--- scripts/index.js
+++ scripts/index.js
@@ -5,11 +5,16 @@ var render = function (template, node) {
 document.addEventListener("DOMContentLoaded", function(event){
 
     let field = document.getElementById("note");
-    if (localStorage.getItem("autosave")) {
-      field.value = localStorage.getItem("autosave");
-    }
+    chrome.storage.local.get(['autosave'], function(result) {
+      // console.log('Value currently is ' + result.autosave);
+      if (result.autosave !== undefined) {
+      	field.value = result.autosave;
+      }
+    });
     field.addEventListener("change", function() {
-      localStorage.setItem("autosave", field.value);
+      chrome.storage.local.set({autosave: field.value}, function() {
+          // console.log('Value is set to ' + field.value);
+        });
     });
 
 });
